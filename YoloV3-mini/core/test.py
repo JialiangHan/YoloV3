cb7c761ec6aab5a4a1b79d8666ace5402501fb31
Jialiang Han <hanjiali@umich.edu>
2021-06-18 22:32:15 +0800

    add predict_set.py to make output has the same channel number

--- YoloV3-mini/core/test.py
+++ YoloV3-mini/core/test.py
@@ -3,7 +3,7 @@ import backbone
 from concat_module import concat_module
 from encoder_decoder_module import encoder_decoder_module
 from feature_fusion_module import feature_fusion
-
+from predict_set import predict_set
 
 def main():
     input_data = tf.ones([1, 416, 416, 3])
@@ -19,7 +19,16 @@ def main():
     output_feature_fusion_2 = f2(output_concat_2, output_encoder_2)
     f3 = feature_fusion(384, 16)
     output_feature_fusion_3 = f3(output_concat_3, output_encoder_3)
-    print(output_feature_fusion_1.shape, output_feature_fusion_2.shape, output_feature_fusion_3.shape)
+    p1 = predict_set(output_feature_fusion_1.shape[-1],255)
+    p2 = predict_set(output_feature_fusion_2.shape[-1],255)
+    p3 = predict_set(output_feature_fusion_3.shape[-1],255)
+    final_output_1 = p1(output_feature_fusion_1)
+    final_output_2 = p2(output_feature_fusion_2)
+    final_output_3 = p3(output_feature_fusion_3)
+    print(final_output_1.shape)
+    print(final_output_2.shape)
+    print(final_output_3.shape)
+    
 
 
 if __name__ == '__main__':
