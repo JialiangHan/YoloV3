dc8bd2d9691262f9fdcbc0c6520961989f1e2155
Jialiang Han <hanjiali@umich.edu>
2021-06-18 23:58:38 +0800

    make YoloV3_mini.py

--- YoloV3-mini/core/test.py
+++ YoloV3-mini/core/test.py
@@ -5,7 +5,7 @@ from YoloV3_mini import YoloV3_mini
 def main():
     input_data = tf.ones([1, 416, 416, 3])
     y = YoloV3_mini(255)
-    y.build(input_shape=[1,416,416,3])
+    y.build(input_shape=[1, 416, 416, 3])
     y.summary()
     final_output_1, final_output_2, final_output_3 = y(input_data)
 
